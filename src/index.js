86aa4b1743dc0ff3d576c5d2596f0075b64b4ebf
Meri <mgogi13@freeuni.edu.ge>
2022-01-31 15:13:07 +0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>Hi</React.StrictMode>,
+  document.getElementById('root')
+);
