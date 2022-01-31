9b9ba0152368a79eb9d50702ab332dcc351eeb36
Meri <mgogi13@freeuni.edu.ge>
2022-01-31 20:13:04 +0400

    Setup Project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>Hi</React.StrictMode>,
-  document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <div>hi</div>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
